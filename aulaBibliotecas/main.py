7ebfb4bd8a04a89d01bb445e958c3c27d980696a
Miguel Avila <miguelaviladeoliveira@gmail.com>
2022-07-25 15:01:40 -0300

    aula do dia 25/07

---
+++ aulaBibliotecas/main.py
@@ -0,0 +1,10 @@
+import module
+
+
+def main():
+    x = module.raiz(4, 2)
+    print(x)
+
+
+if __name__ == "__main__":
+    main()
---
+++ aulaBibliotecas/module/__init__.py
@@ -0,0 +1,22 @@
+def soma(a: int, b: int) -> int:
+    return a + b
+
+
+def subt(a: int, b: int) -> int:
+    return a - b
+
+
+def mult(a: int, b: int) -> int:
+    return a * b
+
+
+def div(a: int, b: int) -> float:
+    return a / b
+
+
+def exp(a: int, b:int) -> int:
+    return a ** b
+
+
+def raiz(a: int, b: int) -> float:
+    return a ** (1 / b)
